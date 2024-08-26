a179477d18db984862a07c3db6f8f448e37b8773
Toyemaker <cyames.studios@gmail.com>
2024-08-25 21:24:48 -0400

    Exploratory Analysis - US Census Data

--- exploratory_analysis_food_bank.R
+++ exploratory_analysis_food_bank.R
@@ -2,18 +2,36 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 
-mtmg_xlsx = read_excel("./R/data/Map_The_Meal_Gap.xlsx")
-zip_to_coords = read.csv("./R/data/zip_lat_long.csv")
-zip_to_fip = read.csv("./R/data/ZIP-COUNTY-FIPS_2017-06.csv")
-federal_codes = read.table("./R/data/FederalCodes_GA.txt", 
+mtmg_xlsx = read_excel("./data/Map_The_Meal_Gap.xlsx")
+federal_codes = read.table("./data/FederalCodes_GA.txt", 
                            sep = "|", 
                            header = TRUE,
                            quote = "")
 
-mtmg_county = read_excel("./R/data/Map_The_Meal_Gap.xlsx", sheet = "County")
+mtmg_county = read_excel("./data/Map_The_Meal_Gap.xlsx", sheet = "County")
 glimpse(mtmg_county)
 
-glimpse(federal_codes)
+mtmg_service_region = mtmg_county %>%
+  filter((`Member 1 ID` == 324 | FIPS == 13105)) %>%
+  select(!contains(c("Ratio", "Member", "Census", "Continuum"))) %>%
+  group_by(`County, State`, Year)
+
+ggplot(mtmg_service_region, aes(factor(Year), `Overall Food Insecurity Rate`, fill = factor(Year))) +
+  geom_bar(stat = "identity") +
+  geom_hline(yintercept = 0.1) +
+  facet_wrap(~ `County, State`)
+
+education_data = read_excel("./data/ACSST1Y2022.S1501-2024-08-26T001853.xlsx", sheet = "Data")
+
+
+
+
+
+
+
+
+
+
 
 georgia_federal_data = federal_codes %>%
   filter(census_class_code %in% c("H1", "H6")) %>%
